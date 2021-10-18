fcd8955247e88a11aad709eec2d3bc8671bdec14
DruboSaptapan <saptapan2017@gmail.com>
2021-10-18 20:20:51 +0600

    contact-form updated

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 import map from './images/maps.png'
 
-import { FcGoogle } from "react-icons/fc";
-import { VscGithubAlt } from "react-icons/vsc";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
@@ -24,25 +22,18 @@ const Login = () => {
 
                             <div className="form-floating mb-3">
                                 <input type="text" className="form-control" id="floatingInput" placeholder="Rudra Barua" />
-                                <label htmlFor="floatingInput">Password</label>
+                                <label htmlFor="floatingInput">Your name</label>
                             </div>
 
                             <div className="form-floating">
                                 <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: 100}}></textarea>
-                                <label htmlFor="floatingTextarea2">Comments</label>
+                                <label htmlFor="floatingTextarea2">Type your message here...</label>
                             </div>
 
                             <div className="pb-2">
-                                <button className="btn border w-100 fw-normal mt-2"><FcGoogle /> Log in with Google</button>
-                            </div>
-                            <div className="sideline">OR</div>
-                            <div>
-                                <button className="btn btn-outline-dark w-100 fw-normal mt-2"> <VscGithubAlt /> Log in With Github</button>
+                                <button className="btn btn-primary w-100 fw-normal mt-2 rounded">Submit</button>
                             </div>
                         </form>
-                        <div className="pt-4 text-center">
-                            New to Fetoskope <Link to="/register" style={{ textDecoration: 'none' }}>Register</Link>
-                        </div>
                     </div>
                 </div>
             </div>
