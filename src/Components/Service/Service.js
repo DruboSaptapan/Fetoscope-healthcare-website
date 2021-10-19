3e8c0945f84f83a7104c0b5b56b8835ed23a3423
DruboSaptapan <saptapan2017@gmail.com>
2021-10-19 08:34:35 +0600

    service-description added

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Service = (props) => {
     const {service} = props;
-    const {name, image} = service;
+    const {name, image, description} = service;
     return (
         <div className="col">
         <div className="card h-100 py-3 px-4 d-flex flex-column align-items-center">
           <img src={image} className="card-img-top" alt="..."/>
-          <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <div className="card-body px-0">
+            <h5 className="card-title fw-bold">{name}</h5>
+            <p className="card-text text-start">{description.slice(0, 175)}</p>
           </div>
         </div>
       </div>
