83cbfa7abcb6a0b669d0af969baed3ea4a1d7efa
DruboSaptapan <saptapan2017@gmail.com>
2021-10-20 15:49:43 +0600

    github login complete

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,53 +1,109 @@
-import { useEffect, useState } from 'react'
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, GithubAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
 import initializeAuthentication from '../Firebase/firebase.init';
 
-initializeAuthentication()
+initializeAuthentication();
 
 const useFirebase = () => {
     const [user, setUser] = useState({});
-
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
 
     const auth = getAuth();
     const googleProvider = new GoogleAuthProvider();
     const githubProvider = new GithubAuthProvider();
 
     const signInUsingGoogle = () => {
-        // signInWithPopup(auth, googleProvider)
-        // .then(result =>{
-        //     const googleUser = result.user;
-        //     console.log(googleUser)
-        // })
-        return signInWithPopup(auth, googleProvider)
+        setIsLoading(true);
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                setUser(result.user);
+            })
+            .finally(() => setIsLoading(false));
+    };
+
+    /* github signing in */
+    const signInUsingGithub = () => {
+        setIsLoading(true);
+        signInWithPopup(auth, githubProvider)
+            .then(result => {
+                console.log(result.user);
+                setUser(result.user);
+            })
+            .finally(() => setIsLoading(false));
     }
 
-    const signInUsingGithub = () => {
-        // signInWithPopup(auth, githubProvider)
-        // .then(result =>{
-        //     const githubUser = result.user;
-        //     console.log(githubUser)
-        // })
-        return signInWithPopup(auth, githubProvider)
+    const handleEmailChange = e => {
+        setEmail(e.target.value);
+    }
+    const handlePasswordChange = e => {
+        setPassword(e.target.value)
+    }
+    const handleRegistration = e => {
+        e.preventDefault();
+        if (password.length < 6) {
+            setError('Password At Least 6 Character')
+            return;
+        }
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setError('Password Must contain 2 upper case');
+            return;
+        }
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+            })
+            .catch(error => {
+                setError(error.message)
+            });
+    }
+
+    const handleSignIn = (email, password) => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                setError(error.message);
+            })
     }
 
     const logOut = () => {
+        setIsLoading(true);
         signOut(auth)
-        .then(() => {
-            setUser({})
-        })
-        .catch(error =>{})
-    }
+            .then(() => { })
+            .finally(() => setIsLoading(false));
+    };
 
+    // it observes that user changes state when user sign in, sign out
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
+        const unsubscribed = onAuthStateChanged(auth, user => {
             if (user) {
-                setUser(user)
+                setUser(user);
             }
+            else {
+                setUser({})
+            }
+            setIsLoading(false);
         });
-        return unsubscribe;
-    }, [])
+        return () => unsubscribed;
+    }, []);
 
-    return { user, signInUsingGoogle, signInUsingGithub, logOut }
-}
+    return {
+        user,
+        error,
+        isLoading,
+        logOut,
+        signInUsingGoogle,
+        signInUsingGithub,
+        handleEmailChange,
+        handlePasswordChange,
+        handleRegistration,
+        handleSignIn
+    }
+};
 
 export default useFirebase;
